1338ccca76c5c32f1fd9aa7bf50d7c8b6d6aa2d4
angelfacundotorres <torresangelfacundo@gmail.com>
2022-10-13 23:33:26 -0300

    finalizacion de los ejercicios

--- app.js
+++ app.js
@@ -49,4 +49,42 @@
 // }
 
 // 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
-let frase = prompt('Ingresa una frase');
+// let frase = prompt('Ingresa una frase');
+
+// for (let i = 0; i < frase.length; i++) {
+//     if (frase.charAt(i) === 'a' || frase.charAt(i) === 'á' || frase.charAt(i) === 'e' || frase.charAt(i) === 'é' || frase.charAt(i) === 'i' || frase.charAt(i) === 'í' || frase.charAt(i) === 'o' || frase.charAt(i) === 'ó' || frase.charAt(i) === 'u' || frase.charAt(i) === 'ú') {
+//         document.write(frase.charAt(i));
+//     }    
+// }
+
+// 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+// let dividendo = parseInt(prompt('Ingresa un número y veremos si es divisible por 2, 3, 5 o 7'));
+// if (dividendo % 2 === 0 || dividendo % 3 === 0 || dividendo % 2 === 5 || dividendo % 2 === 7) {
+//     document.write('Si es divisible por unos de los números anteriormente mencionados');    
+// } else {
+//     document.write('El número ingresado no es divisible ')
+// }
+
+// 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+// let number = parseInt(prompt('Ingresa un número'))
+
+// if (number % 2 === 0 || number % 3 === 0 || number % 5 === 0 || number % 7 === 0) {
+//   if (number % 2 === 0) {
+//     document.write('Es divisible por 2')
+//   }
+//   if (number % 3 === 0) {
+//     document.write('Es divisible por 3')
+//   }
+//   if (number % 5 === 0) {
+//     document.write('Es divisible por 5')
+//   }
+//   if (number % 7 === 0) {
+//       document.write('Es divisible por 7')
+//   }  
+// } else {
+//   document.write('No es divisible por ninguno de los posibles divisores')
+// }
+
+
+
+
